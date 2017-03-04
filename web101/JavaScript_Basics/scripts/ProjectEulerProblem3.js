5d24e633d9ac50974904e9255a06686c12aaad3c
ericaddison <ericaddison@gmail.com>
2017-03-04 12:14:48 -0600

    Problem 3 works

--- web101/JavaScript_Basics/scripts/ProjectEulerProblem3.js
+++ web101/JavaScript_Basics/scripts/ProjectEulerProblem3.js
@@ -21,8 +21,35 @@ var run_p3 = function(){
 
 };
 
-// Sum of even fib numbers fuction
+// find the biggest prime factor of a value n
 var biggestPrimeFactor = function(n){
+  //for(var i=Math.floor(n/2); i>=2; i--){
+  //  if(isFactor(i,n) && isPrime(i))
+  //    return i;
+  //}
+  biggestPrime = 1;
+  for(var i=2; i<=Math.floor(Math.sqrt(n)); i++){
+    if(isFactor(i,n)){
+      var p1 = n/i;
+      if(isPrime(p1) && p1>biggestPrime)
+        biggestPrime = p1;
+      else if(isPrime(i) && i>biggestPrime)
+        biggestPrime = i;
+    }
 
-  return "Looking for biggest prime factor of " + n;
+  }
+  return biggestPrime;
+}
+
+// is n a factor of m?
+var isFactor = function(n,m){
+  return m%n===0;
+}
+
+var isPrime = function(n){
+  for(var i=2; i<=Math.sqrt(n); i++){
+    if(n%i === 0)
+      return false;
+  }
+  return true;
 }
