f01931d33d397c54afd5889054f98aa91577332e
ericaddison <ericaddison@gmail.com>
2017-03-06 08:04:06 -0600

    p5 input checks in place

--- web101/JavaScript_Basics/scripts/ProjectEulerProblem5.js
+++ web101/JavaScript_Basics/scripts/ProjectEulerProblem5.js
@@ -3,15 +3,31 @@
 // BRUTE FORCE ... clearly need a more clever way!
 
 var run_p5 = function(){
-  console.log("In p5!");
 
   var inputSpan = document.getElementById('p5InputSpan');
   var input1 = document.getElementById('p5Input');
   var resultDiv = document.getElementById('p5Result');
   var value = (input1.value);
 
-  resultDiv.innerHTML = "Result: " + smallestMultiple(value);
+  var MAX_N = 30;
+  var MIN_N = 2;
 
+  if(!isNumeric(value)){
+    resultDiv.innerHTML = "Error: Please enter a numeric value...";
+    setErrorBorder(resultDiv);
+    setErrorFont(inputSpan);
+  }
+  else if(value<MIN_N || value>MAX_N){
+    resultDiv.innerHTML = "Error: Please enter an integer between "
+    + MIN_N + " and " + MAX_N + "...";
+    setErrorBorder(resultDiv);
+    setErrorFont(inputSpan);
+  }
+  else {
+    resultDiv.innerHTML = "Result: " + smallestMultiple(value);
+    setSuccessBorder(resultDiv);
+    setAutoFont(inputSpan);
+  }
 
 }
 
